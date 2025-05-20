39cd7c21886e21ff32e12dbb0c23548b56262928
Natasha <natasha.marie33@gmail.com>
2025-05-20 19:08:08 +0900

    made some minor adjustments

--- app/modules/History.tsx
+++ app/modules/History.tsx
@@ -20,7 +20,7 @@ const rock = RocknRoll_One({
 })
 
 
-const HistoryMobile = () => {
+const History = () => {
 
   return (
     <section id="history" className="h-screen overflow-hidden">
@@ -38,15 +38,15 @@ const HistoryMobile = () => {
 
                     {/*fadeIn history details*/}
                     <div className={`${mplus.className} text-stone-300 absolute inset-0  pointer-events-none z-30`}>
-                      <h4 className="absolute text-right text-2xl sm:text-4xl left-[29%] top-[5%] sm:left-[31%] sm:top-[6%]"><BounceEffect delay={0.6}><span className="text-sm sm:text-xl">2013年</span><br />大学卒</BounceEffect></h4>
+                      <h4 className="absolute text-right text-2xl sm:text-4xl left-[29%] top-[5%] sm:left-[31%] sm:top-[5%]"><BounceEffect delay={0.6}><span className="text-sm sm:text-xl">2013年</span><br />大学卒</BounceEffect></h4>
 
-                      <h4 className="absolute text-left text-2xl sm:text-4xl top-[23%] right-[10%] sm:top-[21%] sm:right-[15%]"><BounceEffect delay={0.8}><span className="text-sm sm:text-xl">2014年</span><br />英語教師</BounceEffect></h4>
+                      <h4 className="absolute text-left text-2xl sm:text-4xl top-[23%] right-[10%] sm:top-[21%] sm:right-[17%]"><BounceEffect delay={0.8}><span className="text-sm sm:text-xl">2014年</span><br />英語教師</BounceEffect></h4>
 
                       <h4 className="absolute text-2xl sm:text-4xl text-right top-[39%] left-[8%] sm:top-[39%] sm:left-[15%]"><BounceEffect delay={1}><span className="text-sm sm:text-xl">2016年</span><br />映像撮影<br />編集<br /></BounceEffect></h4>
 
                       <h4 className="absolute text-left text-2xl sm:text-4xl bottom-[28%] right-[13%] sm:bottom-[28%] sm:right-[16%]"><BounceEffect delay={1.2}><span className="text-sm sm:text-xl">2017年</span><br />ゲーム業界で<br />日英翻訳者</BounceEffect></h4>
 
-                      <h4 className="absolute text-left text-2xl sm:text-4xl bottom-[9%] left-[19%]"><BounceEffect delay={1.4}><span className="text-sm sm:text-xl">2024年</span><br />フロントエンドエンジニア</BounceEffect></h4>
+                      <h4 className="absolute text-left text-2xl sm:text-4xl bottom-[8%] left-[19%]"><BounceEffect delay={1.4}><span className="text-sm sm:text-xl">2024年</span><br />フロントエンドエンジニア</BounceEffect></h4>
                     </div>
                 </div>
           </div>
@@ -81,57 +81,4 @@ const HistoryMobile = () => {
   )
 }
 
-export default HistoryMobile
-
-
-
-
-
-// {/* <div className="block md:hidden relative">
-            
-//                 <div className="relative w-full h-full flex items-center justify-center">
-//                     <div className="relative w-full h-[vh]">
-//                         <HistorySvgMaskVerticle className="relative h-screen w-full z-20 " />
-//                         <HistorySvgVerticle className="absolute h-screen inset-0 z-10 w-full text-glow" />
-//                         <HistorySvgMaskHORIZONTAL className="relative w-screen h-full z-20 " />
-//                         <HistorySvgHORIZONTAL className="absolute w-screen inset-0 z-10 h-full text-glow" />
-//                     </div>
-
-//                     {/*fadeIn history details*/}
-//                     <div className={`${mplus.className} text-stone-300 absolute inset-0  pointer-events-none z-30`}>
-//                       <h4 className="absolute text-right text-2xl sm:text-4xl left-[29%] top-[5%] sm:left-[31%] sm:top-[6%]"><BounceEffect delay={0.6}><span className="text-sm sm:text-xl">2013年</span><br />大学卒</BounceEffect></h4>
-
-//                       <h4 className="absolute text-left text-2xl sm:text-4xl top-[23%] right-[10%] sm:top-[21%] sm:right-[15%]"><BounceEffect delay={0.8}><span className="text-sm sm:text-xl">2014年</span><br />英語教師</BounceEffect></h4>
-
-//                       <h4 className="absolute text-2xl sm:text-4xl text-right top-[39%] left-[8%] sm:top-[39%] sm:left-[15%]"><BounceEffect delay={1}><span className="text-sm sm:text-xl">2016年</span><br />映像撮影<br />編集<br /></BounceEffect></h4>
-
-//                       <h4 className="absolute text-left text-2xl sm:text-4xl bottom-[28%] right-[13%] sm:bottom-[28%] sm:right-[16%]"><BounceEffect delay={1.2}><span className="text-sm sm:text-xl">2017年</span><br />ゲーム業界で<br />日英翻訳者</BounceEffect></h4>
-
-//                       <h4 className="absolute text-left text-2xl sm:text-4xl bottom-[9%] left-[19%]"><BounceEffect delay={1.4}><span className="text-sm sm:text-xl">2024年</span><br />フロントエンドエンジニア</BounceEffect></h4>
-//                     </div>
-//                 </div>
-//           </div>
-//           {/*end*/}
-
-//           {/*md and lg screens*/}
-//           <div className="hidden md:block relative">
-
-//                     <div className="relative w-full h-full right-[15px] flex items-center justify-center">
-//                         <HistorySvgMaskHORIZONTAL className="relative w-screen h-full z-20 " />
-//                         <HistorySvgHORIZONTAL className="absolute w-screen inset-0 z-10 h-full text-glow" />
-//                     </div>
-
-//                     {/*fadeIn history details*/}
-//                     <div className={`${mplus.className} absolute text-stone-300 inset-0  pointer-events-none z-30`}>
-
-//                       <h4 className="absolute text-right md:right-[4%] md:top-[62%] md:text-2xl lg:right-[6%] lg:top-[62%] lg:text-3xl"><BounceEffect delay={0.6}><span className="md:text-lg lg:text-xl">2013年</span><br />大学卒</BounceEffect></h4>
-
-//                       <h4 className="absolute text-right md:right-[23%] md:top-[22%] md:text-2xl lg:right-[23%] lg:top-[28%] lg:text-3xl"><BounceEffect delay={0.8}><span className="md:text-lg lg:text-xl">2014年</span><br />英語教師</BounceEffect></h4>
-
-//                       <h4 className="absolute text-left md:left-[42%] md:top-[53%] md:text-2xl lg:left-[43%] lg:top-[53%] lg:text-3xl"><BounceEffect delay={1}><span className="md:text-lg lg:text-xl">2016年</span><br />映像撮影・編集<br /></BounceEffect></h4>
-
-//                       <h4 className="absolute text-left md:left-[10%] md:top-[14%] md:text-2xl lg:left-[11%] lg:top-[17%] lg:text-3xl"><BounceEffect delay={1.2}><span className="md:text-lg lg:text-xl">2017年</span><br />ゲーム業界で日英翻訳者</BounceEffect></h4>
-
-//                       <h4 className="absolute text-left md:left-[14%] md:bottom-[3%] md:text-2xl lg:left-[14%] lg:bottom-[3%] lg:text-3xl"><BounceEffect delay={1.4}><span className="md:text-lg lg:text-xl">2024年</span><br />フロントエンドエンジニア</BounceEffect></h4>
-//                     </div>
-//           </div> */}
+export default History
